e96fc083925affdac0e52367d68b03f95907bbba
itsalaakhaled1-maker <itsalaakhaled1@gmail.com>
2026-07-27 18:44:11 +0300

    addcity2iata

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.alrahhal.app',
+  appName: 'الرحّال',
+  webDir: 'out'
+};
+
+export default config;
